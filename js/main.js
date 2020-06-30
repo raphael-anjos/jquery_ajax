cc26d2e7a34b43da8297a5bf7825ebd85f864201
raphael-anjos <45611743+raphael-anjos@users.noreply.github.com>
2020-06-29 23:52:11 -0300

    projeto que coleta dados de uma API sobre casos de Covid-19 no brasil, utilizando jquery+ajax

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function consultaUf(){
+    var uf = document.getElementById("uf").value.toUpperCase();
+    var url = "https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/"+uf;
+    
+
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+
+            $("#estadoUf").html(response.uf);
+            $("#estado").html(response.state);
+            $("#casos").html(response.cases);
+            $("#mortes").html(response.deaths);
+            $("#suspeitos").html(response.suspects);
+            $("#titulo_uf").html("Casos de Covid-19 no estado de: " + response.state);
+        }
+    })
+}
